8802b08cb6fc35c3ad5e16981fcc5acbcecd1544
Gabriel José Lemache de Castro <69813312+lemachegabriel@users.noreply.github.com>
2021-04-21 16:39:04 -0300

    Create Missing_Number.cpp
    You are given all numbers between 1,2,…,n except one. Your task is to find the missing number.
    
    Input
    
    The first input line contains an integer n.
    
    The second line contains n−1 numbers. Each number is distinct and between 1 and n (inclusive).
    
    Output
    
    Print the missing number.
    
    Constraints
    
        2≤n≤2⋅105
    
    
    Example
    
    Input:
    5
    2 3 1 5
    
    Output:
    4

---
+++ Missing_Number.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    long long n, s=0;
+    cin >> n;
+    for(int i=1; i<n; i++) { 
+        int a;
+        cin >> a;
+        s+=a;
+    }
+    cout << n*(n+1)/2-s;
+    
+}
